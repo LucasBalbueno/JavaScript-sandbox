c85581f68d8d2e7435f2f9289da2797eb7aa12b5
Lucas Balbueno Vicencio <182300633@senacinformatica.br>
2023-11-06 21:12:49 -0300

    Começando exercícios de lógica da aula Senac

---
+++ Treinando-logica-basica/exerciciosSenac/exercícios.js
@@ -0,0 +1,28 @@
+// 01) Fazer um programa que imprima a média aritmética dos números 8,9 e 7. A média dos números 4, 5 e 6. A soma das duas médias.
+
+// 02) Ler um ano de nascimento e ano atual. Imprimir a idade da pessoa. Se a idade for maior ou igual a 18 leia o nome da pessoa e imprima o nome digitado e uma mensagem informando que sua entrada é permitida. (Ex: Fulano, sua entrada foi permitida.)
+
+// 03) Solicitar salário, prestação. Se prestação for maior que 20% do
+// salário, imprimir : Empréstimo não pode ser concedido. Senão
+// imprimir Empréstimo pode ser concedido.
+
+// 04) Informar um saldo e imprimir o saldo com reajuste de 1%.
+
+// 05) Informar um número e imprimir se é par ou ímpar.
+
+// 06) Ler 1 número. Se positivo, imprimir raiz quadrada senão o
+// quadrado do número.
+
+// 07) Ler um número e imprimir igual a 20, menor que 20, maior que
+// 20.
+
+// 08) Crie um algoritmo que receba 3 números e informe qual o maior
+// entre eles.
+
+// 09) Faça um algoritmo que leia dois números nas variáveis NumA e
+// NumB, nessa ordem, e imprima em ordem inversa, isto é, se os
+// dados lidos forem NumA = 5 e NumB = 9, por exemplo, devem ser
+// impressos na ordem NumA = 9 e NumB = 5.
+
+// 10) Faça um algoritmo que leia dois números e indique se são iguais
+// ou se são diferentes. Mostre o maior e o menor (nesta sequência).
